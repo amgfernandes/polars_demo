c19bdaae22b63f1d3a9d222b97798923c9cca519
amgfernandes <amgfernandes@gmail.com>
2023-01-21 15:47:01 +0100

    testing polars

--- polars_demo1.py
+++ polars_demo1.py
@@ -1,28 +1,32 @@
 #%%
 import polars as pl
+import seaborn as sns
 # %%
 df = pl.read_csv("https://j.mp/iriscsv")
 print(df.filter(pl.col("sepal_length") > 5)
       .groupby("species", maintain_order=True)
       .agg(pl.all().sum())
 )
-
+#%%
+df = (pl.read_parquet('https://raw.githubusercontent.com/RobinL/iris_parquet/main/gridwatch/gridwatch_2023-01-08.parquet')
+    .lazy()
+)
+# %%
+print(df.collect().shape)
+df.collect().head()
 # %%lazy load
-
-
 df = pl.read_csv("https://j.mp/iriscsv").lazy()
 print(df.filter(pl.col("sepal_length") > 5)
       .groupby("species", maintain_order=True)
       .agg(pl.all().sum())
 )
 # %%
-df.collect()
+df.collect().head(10)
+# %% read parquet
+df.collect().write_csv('output.csv')
 # %%
-
-df = (pl.read_parquet('https://raw.githubusercontent.com/RobinL/iris_parquet/main/gridwatch/gridwatch_2023-01-08.parquet')
-    .lazy()
-)
+df.collect().write_parquet('output.parquet')
 # %%
-df= df.collect()
-print(df.shape)
+pd_df=df.collect().to_pandas()
+sns.displot(data=pd_df)
 # %%
